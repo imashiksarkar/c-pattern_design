3b635650e408edd1c8fd52a6a27e848edf265708
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2021-10-25 12:36:52 +0600

    added diamond printing pattern

---
+++ diamond.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    /*
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+*/
+
+    int n = 9;
+    int number_of_spaces_to_print = ((int)ceil((float)n / (float)2) * 2 - 2);
+    int number_of_stars_to_print = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int s = 1; s <= number_of_spaces_to_print; s++)
+        {
+
+            printf(" ");
+        }
+
+        for (int j = 1; j <= number_of_stars_to_print; j++)
+        {
+
+            printf("* ");
+        }
+
+        if (i <= n / 2)
+        {
+            number_of_spaces_to_print -= 2;
+            number_of_stars_to_print += 2;
+        }
+        else
+        {
+            number_of_spaces_to_print += 2;
+            number_of_stars_to_print -= 2;
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
